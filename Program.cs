0237f619be8806d478ef7545b4c1bb3b4e4ef73e
sanshanthan <sanshanthan@yahoo.com>
2019-12-23 07:29:18 -0800

    GET TOKEN FROM ECM

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ namespace Consume3rdPartyAPI
         {
              //TO CHECK CONNECTIVITY
             HttpGetSuccessfullyconnectedToAtlasECMMessage();
+
+            //TO GET TOKEN FROM ECM
+            GETokenFromAtlasECM();
         }
 
         //TO CHECK CONNECTIVITY
@@ -25,6 +28,30 @@ namespace Consume3rdPartyAPI
             StreamReader reader = new StreamReader(respofnse.GetResponseStream(), Encoding.UTF8);
             var respofnskuie = reader.ReadToEnd();
             Console.WriteLine(respofnskuie);
+        }
+
+        //TO GET TOKEN FROM ECM
+        public static void GETokenFromAtlasECM()
+        {
+            string Tokenuri = "https://webserverdev.iitvision.com:8493/darbarAPI/oauth/token";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Tokenuri);
+            request.Method = "POST";
+            string postData = "grant_type=client_credentials&client_id=evis&client_secret=g63EgQhyuMG7u8tT";
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] bytes = encoding.GetBytes(postData);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = bytes.Length;
+            //1.1 Get the response content
+            Stream newStream = request.GetRequestStream();
+            newStream.Write(bytes, 0, bytes.Length);
+            HttpWebResponse respofnse = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(respofnse.GetResponseStream(), Encoding.UTF8);
+            var respofnskuie = reader.ReadToEnd();
+            dynamic obj = JsonConvert.DeserializeObject(respofnskuie);
+            var AccessTokenValue = obj.access_token;
+            accessToken = AccessTokenValue;
+            Console.WriteLine(AccessTokenValue);
         }
     }
 }
