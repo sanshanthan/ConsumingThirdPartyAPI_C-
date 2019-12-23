fe9b5c6e47248feb4c9c0e59da7f39969dfa56bc
sanshanthan <sanshanthan@yahoo.com>
2019-12-23 07:24:57 -0800

    CHECK CONNECTIVITY

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
 
 namespace Consume3rdPartyAPI
 {
@@ -6,7 +9,22 @@ namespace Consume3rdPartyAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             //TO CHECK CONNECTIVITY
+            HttpGetSuccessfullyconnectedToAtlasECMMessage();
+        }
+
+        //TO CHECK CONNECTIVITY
+        public static void HttpGetSuccessfullyconnectedToAtlasECMMessage()
+        {
+            string Tokenuri = "https://webserverdev.iitvision.com:8493/darbarAPI/rest/hello";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Tokenuri);
+            request.Method = "GET";
+            request.ContentType = "application/x-www-form-urlencoded";
+            HttpWebResponse respofnse = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(respofnse.GetResponseStream(), Encoding.UTF8);
+            var respofnskuie = reader.ReadToEnd();
+            Console.WriteLine(respofnskuie);
         }
     }
 }
